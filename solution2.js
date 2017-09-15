05897e9d37c1b6d0d80e0f0c3ec577df17732964
Hunter Davis <huntsd123@yahoo.com>
2017-09-15 12:29:36 -0500

    Update solution2.js

--- solution2.js
+++ solution2.js
@@ -1,15 +1,13 @@
 /*By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.*/
+var sum = 0;
+var j = 1;
 
-+var sum = 0;
- +var j = 1;
- +
- +for(var i = 0; i <= 4000000;){
- +	j = i + j;
- +	i = j + i;
- +
- +		if(i % 2 === 0){
- +		sum = sum + i;
- +	}
- +}
- +
- +	console.log(sum);
+for(var i = 0; i <= 4000000;){
+j = i + j;
+i = j + i;
+ 
+if(i % 2 === 0){
+  sum = sum + i;
+ }
+}
+console.log(sum);
